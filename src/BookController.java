ec93d862866afd4996a2713c394ed447c0eba330
Dombi Gréta <dombigreta96@gmail.com>
2018-11-21 18:53:29 +0100

    added listing books added deletebookbyid method added addbook method

--- src/BookController.java
+++ src/BookController.java
@@ -1,25 +1,97 @@
+import org.w3c.dom.*;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.util.stream.Stream;
+
 public class BookController {
 
-    public static void PrintAllBoks()
+    public static void PrintAllBooks(Document document) throws TransformerException
     {
-        // todo
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.INDENT,"yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount","2");
+
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(System.out);
+
+        transformer.transform(source,result);
     }
 
-    public static void GetBookById()
+    public static void GetBookById(int Id, NodeList Books)
     {
-        // todo
+        if(Books.getLength() > 0 )
+        {
+            for(int i = 0;  i< Books.getLength(); i++)
+            {
+                int id = Integer.parseInt(Books.item(i).getAttributes().getNamedItem("Id").getTextContent());
+                if(id == Id)
+                {
+                    System.out.println(Books.item(i).getTextContent());
+                    return;
+                }
+            }
+            System.out.println("There is no such book in the database!");
+        }
     }
-    public static void addBook()
+    public static void addBook(Book book, Document document)
     {
-        //todo
+        Element booksElement =  document.getDocumentElement();
+        Element bookElement = document.createElement("book");
+        bookElement.setAttribute("Id",String.valueOf(book.getId()));
+
+       booksElement.appendChild(bookElement);
+
+       Element  titleElement = document.createElement("title");
+       bookElement.appendChild(titleElement);
+       Text titleText = document.createTextNode(book.getTitle());
+       titleElement.appendChild(titleText);
+
+       Element  descriptionElement = document.createElement("description");
+       bookElement.appendChild(descriptionElement);
+       Text descriptionText = document.createTextNode(book.getDescription());
+       descriptionElement.appendChild(descriptionText);
+
+       Element  amountAvailableElement = document.createElement("amount-available");
+       bookElement.appendChild(amountAvailableElement);
+       Text amountAvailableText = document.createTextNode(String.valueOf(book.getAmountAvailable()));
+       amountAvailableElement.appendChild(amountAvailableText);
+
+       Element  priceElement = document.createElement("price");
+       bookElement.appendChild(priceElement);
+       Text priceText = document.createTextNode(String.valueOf(book.getPrice()));
+       priceElement.appendChild(priceText);
+
+
     }
 
-    public static void deleteBook()
+    public static int deleteBookById(int Id, NodeList Books)
     {
-        //todo
+        if(Books.getLength() > 0)
+        {
+            for(int i = 0; i < Books.getLength(); i++)
+            {
+
+                int id = Integer.parseInt(Books.item(i).getAttributes().getNamedItem("Id").getTextContent());
+                if(id == Id)
+                {
+                    Node BookToBeRemoved = Books.item(i);
+                    Node ParentBook = BookToBeRemoved.getParentNode();
+                    ParentBook.removeChild(BookToBeRemoved);
+                    return id;
+                }
+            }
+        }
+        return -1;
     }
 
-    public static void updateBook()
+    public static void updateBook(Document document, Book book)
     {
         //todo
     }
--- src/DomParser.java
+++ src/DomParser.java
@@ -1,23 +1,34 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
 
 public class DomParser {
+
+
+    private static String FILE_INPUT_PATH = "C:\\Users\\i5\\IdeaProjects\\XMLAssignment\\xml\\book.xml";
+
     public static void main(String[] args){
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         documentBuilderFactory.setNamespaceAware(true);
         try
         {
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(new File(""));
-
-
-        }catch (ParserConfigurationException | SAXException | IOException e)
+            Document document = documentBuilder.parse(new File(FILE_INPUT_PATH));
+            NodeList Books = document.getElementsByTagNameNS("books","book");
+            BookController.PrintAllBooks(document);
+            BookController.GetBookById(2345,Books);
+            int deletedBookId = BookController.deleteBookById(3456,Books);
+            BookController.addBook(new Book(7898,"Nincs neki","Lorem shit",12,100000),document);
+            BookController.PrintAllBooks(document);
+        }
+        catch (ParserConfigurationException | SAXException | TransformerException | IOException e)
         {
             e.printStackTrace();
         }
